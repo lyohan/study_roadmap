b358a481d121668c92705d638ec6e4d76c812303
lyohan <ian_90@likelion.net>
2022-03-03 14:09:22 +0900

    feat: add carousel

---
+++ pages/detail.tsx
@@ -0,0 +1,146 @@
+import type { NextPage } from 'next'
+
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+import { useRef, useEffect, useState } from 'react'
+import Slider, { Settings } from 'react-slick'
+import _ from 'lodash'
+
+const Detail: NextPage = () => {
+  const settings: Settings = {
+    dots: true,
+    infinite: false,
+    arrows: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '16px',
+    className: 'py-4 bg-yellow-400',
+    adaptiveHeight: true,
+  }
+
+  const [activeRoadMap, setActiveLoadMap] = useState(0)
+	const sliderBox = useRef(null)
+  useEffect(() => {
+    if (sliderBox?.current) {
+      
+      sliderBox.current.addEventListener('scroll', _.throttle((e) => {
+        const clientWidth = sliderBox?.current?.clientWidth
+        console.log('sadfasfd', sliderBox.current.scrollLeft , sliderBox.current.scrollWidth)
+        // let index = Math.round((sliderBox.current.scrollLeft / sliderBox.current.scrollWidth)) * 3
+        let index = sliderBox.current.scrollLeft / (332 - ((clientWidth - 300) / 2))
+        setActiveLoadMap(index)
+        // if (sliderBox.current.scrollLeft < 300) {
+        //   setActiveLoadMap(0)
+        // }
+
+        // if (sliderBox.current.scrollLeft >= 300 && sliderBox.current.scrollLeft < 600) {
+        //   setActiveLoadMap(1)
+        // }
+
+        // if (sliderBox.current.scrollLeft > 600) {
+        //   setActiveLoadMap(2)
+        // }
+      }, 1000))
+    }
+    // console.log('asdf111f', sliderBox?.current?.scrollLeft)
+  }, [])
+  return (
+    <div style={{ maxWidth: '100%' }}>
+      <div>
+        <h3>웹 프론트엔드 경로를 안내합니다.</h3>
+        <h1>기초 학습</h1>
+      </div>
+      <div className="space-x-3">
+        <button onClick={() => {
+						sliderBox?.current?.scrollLeft = 0
+						// sliderBox?.current?.scrollLeft = Math.floor(sliderBox?.current?.scrollWidth * (1 / 3))
+					}}
+          className={activeRoadMap === 0 ? 'bg-yellow-400' : ''}
+        >one</button>
+        <button onClick={() => {
+          // const scrollLeft = Math.floor(scroller.scrollWidth * (i / 4))
+						const clientWidth = sliderBox?.current?.clientWidth
+						// console.log('asdf', clientWidth - 32 - ((clientWidth - 300) / 2))
+            console.log('button one', 332 - ((clientWidth - 300) / 2))
+						sliderBox?.current?.scrollLeft = 332 - ((clientWidth - 300) / 2)
+            // sliderBox?.current?.scrollLeft = 300
+            // sliderBox?.current?.scrollLeft = Math.floor(sliderBox?.current?.scrollWidth * (2 / 3))
+					}}
+          className={activeRoadMap === 1 ? 'bg-yellow-400' : ''}
+          >two</button>
+        <button onClick={() => {
+						const clientWidth = sliderBox?.current?.clientWidth
+						sliderBox?.current?.scrollLeft = (332 - ((clientWidth - 300) / 2)) * 2
+            // sliderBox?.current?.scrollLeft = 600
+            // sliderBox?.current?.scrollLeft = Math.floor(sliderBox?.current?.scrollWidth * (3 / 3))
+					}}
+          className={activeRoadMap === 2 ? 'bg-yellow-400' : ''}
+        >three</button>
+      </div>
+      <div className="flex space-x-4 snap-mandatory snap-x overflow-x-scroll p-4"
+				ref={sliderBox}
+				style={{ scrollBehavior: 'smooth' }}
+			>
+        <div className="snap-center" style={{ width: 300 }}>
+          <div
+            className="bg-gray-200"
+            style={{ width: 300, height: 275 }}
+          ></div>
+          <div
+            className="mt-4 bg-gray-200"
+            style={{ width: 300, height: 275 }}
+          ></div>
+          <div
+            className="mt-4 bg-gray-200"
+            style={{ width: 300, height: 275 }}
+          ></div>
+          <div
+            className="mt-4 bg-gray-200"
+            style={{ width: 300, height: 275 }}
+          ></div>
+        </div>
+        <div className="snap-center" style={{ width: 300 }}>
+          <div
+            className="bg-gray-200"
+            style={{ width: 300, height: 275 }}
+          ></div>
+          <div
+            className="mt-4 bg-gray-200"
+            style={{ width: 300, height: 275 }}
+          ></div>
+          <div
+            className="mt-4 bg-gray-200"
+            style={{ width: 300, height: 275 }}
+          ></div>
+          <div
+            className="mt-4 bg-gray-200"
+            style={{ width: 300, height: 275 }}
+          ></div>
+        </div>
+        <div className="snap-center" style={{ width: 300 }}>
+          <div
+            className="bg-gray-200"
+            style={{ width: 300, height: 275 }}
+          ></div>
+          <div
+            className="mt-4 bg-gray-200"
+            style={{ width: 300, height: 275 }}
+          ></div>
+          <div
+            className="mt-4 bg-gray-200"
+            style={{ width: 300, height: 275 }}
+          ></div>
+          <div
+            className="mt-4 bg-gray-200"
+            style={{ width: 300, height: 275 }}
+          ></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Detail
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,84 +1,56 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import categoryList from '../mocks/categoryList.json'
+import categoryDetail from '../mocks/categoryDetail.json'
+import { useState } from 'react'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [acticeCategory, setActiveCategory] = useState<string | null>(null)
+  const renderCategory = () =>
+    categoryList.map(({ id, name }) => {
+      const isActive = acticeCategory === id
+      return (
+        <button
+          key={id}
+          className={`rounded-2xl bg-gray-100 py-1 px-3 ${
+            isActive ? 'border-2 border-yellow-400' : ''
+          }`}
+          onClick={() => setActiveCategory(acticeCategory ? null : id)}
         >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+          {name}
+        </button>
+      )
+    })
+  return (
+    <div className="mt-10 ml-10">
+      <header>
+        <h1>관심있는 분야가 있으신가요?</h1>
+        <div className="mt-4 space-x-4">{renderCategory()}</div>
+      </header>
+      <main>
+        <section className="mt-6">
+          {acticeCategory && (
+            <>
+              <h3>{categoryDetail[acticeCategory].title}</h3>
+              <p>{categoryDetail[acticeCategory].description}</p>
+            </>
+          )}
+          <section className="mt-6">
+            {acticeCategory && (
+              <Link href='/detail'>
+                <a>
+                  <button
+                    className="bg-yellow-400 py-4 px-10"
+                    // onClick={nextStep}
+                  >
+                    골랐어요
+                  </button>
+                </a>
+              </Link>
+            )}
+          </section>
+        </section>
+      </main>
     </div>
   )
 }
